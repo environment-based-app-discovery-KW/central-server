3e927332b53938582a333f5a12366ccc875ed46e
Ke Wang <453587854@qq.com>
2018-04-13 19:39:15 +0800

    add helper functions to autoload

---
+++ app/Helpers/functions.php
@@ -0,0 +1,10 @@
+<?php
+function join_paths() {
+    $paths = array();
+
+    foreach (func_get_args() as $arg) {
+        if ($arg !== '') { $paths[] = $arg; }
+    }
+
+    return preg_replace('#/+#','/',join('/', $paths));
+}
