94e4e82bffd580f2e3d3040cec8386959172a650
Ke Wang <453587854@qq.com>
2018-04-13 19:44:17 +0800

    copy db models from app-repo-server

---
+++ app/WebApp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\WebApp
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereUpdatedAt($value)
+ * @property int $developer_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebApp whereDeveloperId($value)
+ */
+class WebApp extends Model
+{
+    protected $connection = 'app-repo-server';
+}
---
+++ app/WebAppVersion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\WebAppVersion
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $web_app_id
+ * @property string $version
+ * @property string $code_bundle_url
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereCodeBundleUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereVersion($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereWebAppId($value)
+ * @property string $logo_url
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\WebAppVersion whereLogoUrl($value)
+ */
+class WebAppVersion extends Model
+{
+    protected $connection = 'app-repo-server';
+}
