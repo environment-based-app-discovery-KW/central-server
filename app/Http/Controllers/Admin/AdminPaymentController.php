5c0f857e7b5f87e2f0139abfa9bafa384bb5ec57
Ke Wang <453587854@qq.com>
2018-05-11 19:24:36 +0800

    show all payment records

--- app/Http/Controllers/Admin/AdminPaymentController.php
+++ app/Http/Controllers/Admin/AdminPaymentController.php
@@ -19,7 +19,16 @@ class AdminPaymentController extends Controller
     {
         $records = Payment::query()
             ->leftJoin("users", "users.id", "=", "payments.user_id")
-            ->get();
+            ->get([
+                'payments.id as id',
+                'payments.webapp_id as webapp_id',
+                'payments.amount_paid as amount_paid',
+                'users.id as user_id',
+                'users.public_key as user_public_key',
+                'payments.created_at',
+                'payments.order_title',
+                'payments.order_description',
+            ]);
         foreach ($records as $record) {
             $record->webapp = WebApp::find($record->webapp_id);
         }
--- resources/views/payment_records.blade.php
+++ resources/views/payment_records.blade.php
@@ -5,13 +5,56 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bulma.min.css">
     <title>All payments</title>
+    <style>
+        span {
+            vertical-align: top;
+        }
+    </style>
 </head>
 <body>
 <div>
-    @foreach($records as $record)
-        {{$record}}
-    @endforeach
+    <table class="table is-fullwidth">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>WebApp</th>
+            <th>User</th>
+            <th>Amount</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Time</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($records as $record)
+            <tr>
+                <td>{{$record->id}}</td>
+                <td>{{$record->webapp->name}} ({{$record->webapp_id}})</td>
+                <td>
+                    <span title="{{$record->user_public_key}}"
+                          style="max-width: 100px;text-overflow: ellipsis;overflow: hidden;display: inline-block">{{$record->user_public_key}}</span>
+                    <span>({{$record->user_id}})</span>
+                </td>
+                <td>
+                    {{number_format($record->amount_paid/100, 2, '.', '')}}
+                </td>
+                <td>
+                    {{$record->order_title}}
+                </td>
+                <td>
+                    <span title="{{$record->order_description}}"
+                          style="text-overflow: ellipsis;overflow: hidden;display: block">{{$record->order_description}}</span>
+                </td>
+                <td>
+                    {{$record->created_at}}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
 </body>
 </html>
